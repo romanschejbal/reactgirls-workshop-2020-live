773e8912932feb468bfd5a96fcce1dfe3a8f47bf
Roman Schejbal <schejbalroman@gmail.com>
2020-10-31 16:49:24 +0100

    remove redundant photo alt text

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       </div>
       <div className="intro-image">
         <img
-          alt="personal photo"
+          alt="personal"
           src="https://images.unsplash.com/photo-1604115707355-73a0b1a10870?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2135&q=80"
         />
       </div>
